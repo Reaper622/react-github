86b3f0127c8c5b1f4ef4ffd468e11c3e8a4dff45
MacTavish Lee <1064416560@qq.com>
2019-08-18 00:10:32 +0800

    :sparkles: feat: 增加加载组件

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,45 @@
 import App, { Container } from 'next/app'
 import { Provider } from 'react-redux'
+import Router from 'next/router'
+import Link from 'next/link'
 
 import 'antd/dist/antd.css'
 
 import MyContext from '../lib/my-context'
 import Layout from '../components/Layout'
+import PageLoading from '../components/PageLoading'
 
 import testHoc from '../lib/with-redux'
 
 class MyApp extends App {
   state = {
     context: 'value',
+    loading: false
   }
 
+  startLoading = () => {
+    this.setState({
+      loading: true
+    })
+  }
+
+  stopLoading = () => {
+    this.setState({
+      loading: false
+    })
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading)
+    Router.events.on('routeChangeComplete', this.stopLoading)
+    Router.events.on('routeChangeError', this.stopLoading)
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading)
+    Router.events.off('routeChangeComplete', this.stopLoading)
+    Router.events.off('routeChangeError', this.stopLoading)
+  }
 
   static async getInitialProps(ctx) {
     const { Component } = ctx
@@ -32,9 +59,16 @@ class MyApp extends App {
     return (
       <Container>
         <Provider store={reduxStore}>
-          <Layout>  
-              <Component {...pageProps} />
-          </Layout>
+          { this.state.loading ? <PageLoading /> : null}
+            <Layout>
+                <Link href="/">
+                  <a>Index</a>
+                </Link>  
+                <Link href="/detail">
+                  <a>Detail</a>
+                </Link>  
+                <Component {...pageProps} />
+            </Layout>
         </Provider>
       </Container>
     )
