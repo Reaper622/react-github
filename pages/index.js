ebeeff2eefa97536019b4dcbc4480c914d23d1c5
MacTavish Lee <1064416560@qq.com>
2019-08-18 15:24:24 +0800

    :alien: chore: 重构不同端请求api的方式

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,22 @@
-export default () => <span>Index</span>
+import axios from "axios";
+
+const api = require('../lib/api')
+
+function Index() {
+    return <span>Index</span>
+}
+
+Index.getInitialProps = async ({ctx}) => {
+    // const result = await axios.get('/github/search/repositories?q=react')
+    //     .then(res => console.log(res))
+
+    const result = await api.request({
+        url: '/search/repositories?q=react'
+    }, ctx.req, ctx.res)
+
+    return {
+        data: result.data,
+    }
+}
+
+export default Index
--- server/api.js
+++ server/api.js
@@ -1,42 +1,65 @@
 const github_base_url = 'https://api.github.com'
 const axios = require('axios')
 
-module.exports = (server) => {
+const { requestGithub } = require('../lib/api')
+
+module.exports = server => {
     server.use(async (ctx, next) => {
         const path = ctx.path
-        if(path.startsWith('/github/')) {
-            const githubAuth = ctx.session.githubAuth
-            const githubPath = `${github_base_url}${ctx.url.replace('/github/', '/')}`
-
-            const token = githubAuth && githubAuth.access_token
-            let headers = {}
-
-            if(token) {
-                headers['Authorization'] = `${githubAuth.token_type} ${token}`
+        const method = ctx.method
+        if(path.startsWith('/github')) {
+            const session = ctx.session
+            const githubAuth = session && session.githubAuth
+            const headers = {}
+            if (githubAuth && githubAuth.access_token) {
+                headers['Authorization'] = `${githubAuth.token_type} ${githubAuth.access_token}`
             }
+            const result = await requestGithub(method, ctx.url.replace('/github/', '/'), {}, headers)
 
-            try {
-                const result = await axios({
-                    method: 'GET',
-                    url: githubPath,
-                    headers
-                })
-                if(result.status === 200) {
-                    ctx.body = result.data
-                    ctx.set('Content-Type', 'application/json')
-                } else {
-                    ctx.status = result.status
-                    ctx.body = {
-                        success: false
-                    }
-                    ctx.set('Content-Type', 'application/json')
-                }
-            }catch(err) {
-                console.error(err);
-                ctx.set('Content-Type', 'application/json')
-            }
+            ctx.status = result.status
+            ctx.body = result.data
         } else {
-            await next();
+            await next()
         }
     })
 }
+
+// module.exports = (server) => {
+//     server.use(async (ctx, next) => {
+//         const path = ctx.path
+//         if(path.startsWith('/github/')) {
+//             const githubAuth = ctx.session.githubAuth
+//             const githubPath = `${github_base_url}${ctx.url.replace('/github/', '/')}`
+
+//             const token = githubAuth && githubAuth.access_token
+//             let headers = {}
+
+//             if(token) {
+//                 headers['Authorization'] = `${githubAuth.token_type} ${token}`
+//             }
+
+//             try {
+//                 const result = await axios({
+//                     method: 'GET',
+//                     url: githubPath,
+//                     headers
+//                 })
+//                 if(result.status === 200) {
+//                     ctx.body = result.data
+//                     ctx.set('Content-Type', 'application/json')
+//                 } else {
+//                     ctx.status = result.status
+//                     ctx.body = {
+//                         success: false
+//                     }
+//                     ctx.set('Content-Type', 'application/json')
+//                 }
+//             }catch(err) {
+//                 console.error(err);
+//                 ctx.set('Content-Type', 'application/json')
+//             }
+//         } else {
+//             await next();
+//         }
+//     })
+// }
