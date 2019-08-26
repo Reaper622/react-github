4ad530ecd72b1a5645002f08504fd4e53abc3ec0
MacTavish Lee <1064416560@qq.com>
2019-08-26 12:30:38 +0800

    :sparkles: feat: 完善搜索页面分页

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
+import { memo, isValidElement }  from 'react'
 import { withRouter } from 'next/router'
-import { Row, Col, List } from 'antd'
+import { Row, Col, List, Pagination } from 'antd'
 import Link from 'next/link'
 import Router from 'next/router'
+
+import Repo from '../components/Repo'
 
 const api = require('../lib/api')
 
@@ -45,18 +48,30 @@ const SORT_TYPES = [
      fontWeight: 100
  }
 
- const FilterLink = ({ name, query, lang, sort, order }) => {
+ function noop() {
+
+ }
+
+ const per_page = 20
+
+ const FilterLink = memo(({ name, query, lang, sort, order, page }) => {
     let queryString = `?query=${query}`
     if(lang) queryString += `&lang=${lang}`
     if(sort) queryString += `&sort=${sort}&order=${order || 'desc'}`
-     return <Link href={`/search${queryString}`}><a>{name}</a></Link>
- }
+    if(page) queryString += `&page=${page}`
+
+    queryString += `&per_page=${per_page}`
+     return (
+     <Link href={`/search${queryString}`}>
+         {isValidElement(name) ? name : <a>{name}</a>}
+    </Link>)
+ })
 
 
 function Search({ router, repos }) {
 
     const querys = router.query
-    const { sort, order, lang } = router.query
+    const { sort, order, lang, page } = router.query
     
     return (
         <div className="root">
@@ -105,6 +120,26 @@ function Search({ router, repos }) {
                         }}
                     />
                 </Col>
+                <Col span={18}>
+                    <h3 className="repos-title">{repos.total_count} 个仓库</h3>
+                    {
+                        repos.items.map(repo => <Repo repo={repo} key={repo.id} />)
+                    }
+                    <div className="pagination">
+                        <Pagination 
+                            pageSize={per_page}
+                            current={Number(page) || 1}
+                            // Github Api 只返回前1000条数据
+                            total={repos.total_count > 1000 ? 1000 : repos.total_count}
+                            onChange={noop}
+                            itemRender={(page, type, ol) => {
+                                const p = type === 'page' ? page : type === 'prev' ? page - 1 : page + 1
+                                const name = type === 'page' ? page : ol
+                                return <FilterLink {...querys} page={p} name={name} />
+                            }}
+                        />
+                    </div>
+                </Col>
             </Row>
             <style jsx>{`
                         .root {
@@ -113,6 +148,15 @@ function Search({ router, repos }) {
                         .list-header {
                             font-weight: 800;
                             font-size: 16px;
+                        }
+                        .repos-title {
+                            border-bottom: 1px solid #eee;
+                            font-size: 24px;
+                            line-height: 50px;
+                        }
+                        .pagination {
+                            padding: 20px;
+                            text-align: center;
                         }
                 `}</style>
         </div>
@@ -134,6 +178,8 @@ Search.getInitialProps = async ({ctx}) => {
     if(lang) queryString += `+language=${lang}`
     if(sort) queryString += `&sort=${sort}&order=${order || 'desc'}`
     if(page) queryString += `&page=${page}`
+
+    queryString += `&per_page=${per_page}`
     const result = await api.request({
         url: `/search/repositories${queryString}`
     }, ctx.req, ctx.res)
